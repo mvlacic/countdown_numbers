a7f277ff4d911848b66500e24b8537ecff4be332
Mateo Vlacic <mateo.vlacic12@gmail.com>
2022-03-01 11:47:45 +1100

    changed how displayed equations are created

--- countdown.py
+++ countdown.py
@@ -5,26 +5,85 @@ from itertools import combinations as combs
 def get_arithmetic(pair, frac=False):
     '''
     Given a tuple pair of numbers, returns a list of all possible
-    NON-NEGATIVE values that can be gotten using simple arithmetic operations
+    values that can be gotten using simple arithmetic operations
     on the input numbers
+    The function works such that is the inputs are both non-negative, then
+    all the results will also be non-negative
     The frac flag determines if fractional solutions are to be included
     '''
 
     x = max(pair) # Define x as the larger of the two
     y = min(pair) # Define y as the smaller of the two
     if x*y == 0: # If either value is 0
-        if x != y:
-            return [x+y, 0, x-y]
-        else:
-            return [0]
+        return [
+            {
+                "result": 0,
+                "equation": f"{x} * {y} = {0}",
+            },
+            {
+                "result": x-y,
+                "equation": f"{x} - {y} = {x-y}"
+            }
+            ]
     else: # If both values are non-zero
         if frac:
-            return [x+y, x*y, x-y, x/y, y/x]
+            return [
+                {
+                    "result": x+y,
+                    "equation": f"{x} + {y} = {x+y}"
+                },
+                {
+                    "result": x*y,
+                    "equation": f"{x} * {y} = {x*y}"
+                },
+                {
+                    "result": x-y,
+                    "equation": f"{x} - {y} = {x-y}"
+                },
+                {
+                    "result": x/y,
+                    "equation": f"{x} / {y} = {x/y}"
+                },
+                {
+                    "result": y/x,
+                    "equation": f"{y} / {x} = {y/x}"
+                }
+                ]
         else:
             if x%y == 0:
-                return [x+y, x*y, x-y, int(x/y)]
+                return [
+                {
+                    "result": x+y,
+                    "equation": f"{x} + {y} = {x+y}"
+                },
+                {
+                    "result": x*y,
+                    "equation": f"{x} * {y} = {x*y}"
+                },
+                {
+                    "result": x-y,
+                    "equation": f"{x} - {y} = {x-y}"
+                },
+                {
+                    "result": int(x/y),
+                    "equation": f"{x} / {y} = {int(x/y)}"
+                }
+                ]
             else:
-                return [x+y, x*y, x-y]
+                return [
+                {
+                    "result": x+y,
+                    "equation": f"{x} + {y} = {x+y}"
+                },
+                {
+                    "result": x*y,
+                    "equation": f"{x} * {y} = {x*y}"
+                },
+                {
+                    "result": x-y,
+                    "equation": f"{x} - {y} = {x-y}"
+                }
+                ]
 
 def solve(target, smalls, frac=False):
     """
@@ -64,33 +123,12 @@ def solve(target, smalls, frac=False):
             del smallscopy[c[0]]
             del smallscopy[c[1]]
             
-            for num,val in enumerate(newvals): # For each possible operation
-                if solve(target, smallscopy+[val], frac) == 1:
-                    if num==0:
-                        print(smallscopy2[c[0]],'+',smallscopy2[c[1]],'=',val)
-                        operations.append(str(smallscopy2[c[0]])+
-                                          '+'+str(smallscopy2[c[1]])+
-                                          '='+str(val))
-                    elif num==1:
-                        print(smallscopy2[c[0]],'*',smallscopy2[c[1]],'=',val)
-                        operations.append(str(smallscopy2[c[0]])+
-                                          '*'+str(smallscopy2[c[1]])+
-                                          '='+str(val))
-                    elif num==2:
-                        print(smallscopy2[c[0]],'-',smallscopy2[c[1]],'=',val)
-                        operations.append(str(smallscopy2[c[0]])+
-                                          '-'+str(smallscopy2[c[1]])+
-                                          '='+str(val))
-                    elif num==3:
-                        print(smallscopy2[c[0]],'/',smallscopy2[c[1]],'=',val)
-                        operations.append(str(smallscopy2[c[0]])+
-                                          '/'+str(smallscopy2[c[1]])+
-                                          '='+str(val))
-                    else:
-                        print(smallscopy2[c[1]],'/',smallscopy2[c[0]],'=',val)
-                        operations.append(str(smallscopy2[c[1]])+
-                                          '/'+str(smallscopy2[c[0]])+
-                                          '='+str(val))
+            for val in newvals: # For each possible operation
+                result = val["result"]
+                equation = val["equation"]
+                if solve(target, smallscopy+[result], frac) == 1:
+                    print(equation)
+                    operations.append(equation)
                     return 1
                     
                     
