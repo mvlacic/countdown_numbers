9af9e6aa97eef39eaa05d04999d489149189c00c
Mateo Vlacic <mateo.vlacic12@gmail.com>
2022-03-01 20:07:37 +1100

    removed fractional capability

--- countdown.py
+++ countdown.py
@@ -2,90 +2,27 @@ from itertools import combinations as combs
 #Solves the number game from countdown
 #Given any input values
 
-def get_arithmetic(pair, frac=False):
+def get_arithmetic(pair):
     '''
     Given a tuple pair of numbers, returns a list of all possible
-    values that can be gotten using simple arithmetic operations
+    integer values that can be gotten using simple arithmetic operations
     on the input numbers
     The function works such that if the inputs are both non-negative, then
     all the results will also be non-negative
-    The frac flag determines if fractional solutions are to be included
     '''
 
     x = max(pair) # Define x as the larger of the two
     y = min(pair) # Define y as the smaller of the two
-    if x*y == 0: # If either value is 0
-        return [
-            {
-                "result": 0,
-                "equation": f"{x} * {y} = {0}",
-            },
-            {
-                "result": x-y,
-                "equation": f"{x} - {y} = {x-y}"
-            }
-            ]
-    else: # If both values are non-zero
-        if frac:
-            return [
-                {
-                    "result": x+y,
-                    "equation": f"{x} + {y} = {x+y}"
-                },
-                {
-                    "result": x*y,
-                    "equation": f"{x} * {y} = {x*y}"
-                },
-                {
-                    "result": x-y,
-                    "equation": f"{x} - {y} = {x-y}"
-                },
-                {
-                    "result": x/y,
-                    "equation": f"{x} / {y} = {x/y}"
-                },
-                {
-                    "result": y/x,
-                    "equation": f"{y} / {x} = {y/x}"
-                }
-                ]
-        else:
-            if x%y == 0:
-                return [
-                {
-                    "result": x+y,
-                    "equation": f"{x} + {y} = {x+y}"
-                },
-                {
-                    "result": x*y,
-                    "equation": f"{x} * {y} = {x*y}"
-                },
-                {
-                    "result": x-y,
-                    "equation": f"{x} - {y} = {x-y}"
-                },
-                {
-                    "result": int(x/y),
-                    "equation": f"{x} / {y} = {int(x/y)}"
-                }
-                ]
-            else:
-                return [
-                {
-                    "result": x+y,
-                    "equation": f"{x} + {y} = {x+y}"
-                },
-                {
-                    "result": x*y,
-                    "equation": f"{x} * {y} = {x*y}"
-                },
-                {
-                    "result": x-y,
-                    "equation": f"{x} - {y} = {x-y}"
-                }
-                ]
+    results = {
+        x+y: f"{x} + {y} = {x+y}",
+        x*y: f"{x} * {y} = {x*y}",
+        x-y: f"{x} - {y} = {x-y}"
+    }
+    if y != 0 and x%y == 0:
+        results[int(x/y)] = f"{x} / {y} = {int(x/y)}"
+    return results
 
-def solve(target, smalls, frac=False):
+def solve(target, smalls):
     """
     Recursive function that solves the game with target value of 'target'
     and available values stored in the list 'smalls'
@@ -105,25 +42,25 @@ def solve(target, smalls, frac=False):
             # First try by deleting the pair of numbers from the list entirely
             # i.e. not using them at all in the calculations
 
-            if solve(target, newsmalls, frac) == 1:
+            if solve(target, newsmalls) == 1:
                 return 1
             
             # If this doesnt work, try by combining them using arithemtic
             pair = [smalls[i] for i in c]
-            newvals = get_arithmetic(pair, frac)
+            newvals = get_arithmetic(pair)
             
             for val in newvals: # For each possible operation
-                result = val["result"]
-                equation = val["equation"]
-                if solve(target, newsmalls+[result], frac) == 1:
+                result = val
+                equation = newvals[val]
+                if solve(target, newsmalls+[result]) == 1:
                     print(equation)
                     operations.append(equation)
                     return 1
                     
                     
-def countdown(target, smalls, frac=False):
+def countdown(target, smalls):
     global operations
     operations=[]
-    solve(target, smalls, frac)
+    solve(target, smalls)
     return operations
 
